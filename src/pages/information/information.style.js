97d23d154959424567208f7f4d2c8a3f4e076422
Bernagaud Tristan <tristan.bernagaud@etu.univ-orleans.fr>
2023-01-29 16:29:02 +0100

    pretty information

--- src/pages/information/information.style.js
+++ src/pages/information/information.style.js
@@ -26,6 +26,64 @@ export const styles = StyleSheet.create({
         fontSize: 16,
 
     },
+    containerLigne: {
+        flexDirection: 'row',
+        top: 20,
+        marginHorizontal: 60,
+
+
+
+    },
+    containerColonne:{
+        backgroundColor: '#fff',
+        borderRadius: 20,
+        width: 150,
+        height: 150,
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginHorizontal: 20,
+
+    },
+
+    textb: {
+        fontSize: 20,
+        fontWeight: "bold",
+        color: "#61E5F8",
+        textAlign: "center",
+    },
+    back: {
+        borderTopEndRadius: "50%",
+        borderTopStartRadius: "50%",
+        height: "50%",
+        width: "100%",
+        flexDirection: "column",
+        justifyContent: "space-around",
+        alignItems: "center",
+        backgroundColor: '#61E5F8',
+    },
+    title2: {
+        fontSize: 30,
+        fontWeight: "bold",
+        color: "#001833",
+    },
+
+    image:{
+        width: 100,
+        height: 100,
+    },
+
+    containerColonne2:{
+
+        backgroundColor: '#fff',
+        borderRadius: 20,
+        width: 350,
+        height: 150,
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginHorizontal: 20,
+
+    }
+
 
 
 

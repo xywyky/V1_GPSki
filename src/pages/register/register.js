5664f5bdf300dfc2074b3fffa8205ef3d2c7e61f
Bernagaud Tristan <tristan.bernagaud@etu.univ-orleans.fr>
2023-02-03 16:56:56 +0100

    :dizzy: Subscribe pretty

--- src/pages/register/register.js
+++ src/pages/register/register.js
@@ -1,4 +1,4 @@
-import { Text, TextInput, View } from 'react-native'
+import {Image, Pressable, Text, TextInput, View} from 'react-native'
 
 import { ButtonFactory} from "../../components";
 import React from 'react'
@@ -33,56 +33,157 @@ const RegisterPage = ({ navigation }) => {
                     }}>
                         S'inscrire
                     </Text>
-                    <Text style={{
-                        ...styles.colorizedText2,
-                    }}>
 
+                    <Text style={{...styles.colorizedText2,
+                        ...styles.textStyle}}
+                    >
+                        Se connecter avec Apple
+                    </Text>
+                    <Text style={{...styles.colorizedText2,
+                        ...styles.textStyle}}
+                    >
+                        OU
                     </Text>
 
-                    {error && (
-                        <Text style={{ color: "red" }}>Invalid credentials</Text>
-                    )}
-
-                    <TextInput
-                        onChangeText={setEmail}
-                        style={styles.loginInputBox}
-                        value={email}
-                        placeholder="Adresse e-mail"
-                    />
+                    <Text style={{...styles.colorizedText2,
+                        ...styles.textStyle}}
+                    >
+                        Créez un compte ici
+                    </Text>
                 </View>
 
                 <View style={styles.loginInputContainer}>
+                    <View style={styles.ContainerTextInput}>
 
-                    <TextInput
-                        style={styles.loginInputBox}
-                        onChangeText={setPassword}
-                        value={password}
-                        placeholder="Mot de passe"
-                        secureTextEntry
-                    />
-                </View>
-                <View style={styles.loginInputContainer}>
+                        <Image source={require('../../../assets/Profile.png')}/>
+                        <Image style={styles.line_V} source={require('../../../assets/Line_deco_v.png')}/>
+                        <TextInput
+                            onChangeText={setEmail}
+                            style={styles.loginInputBox}
+                            value={email}
+                            placeholder="Prénom"
+                        />
+                    </View>
+                    <Image style={styles.line} source={require('../../../assets/Line_deco.png')}/>
+                    <View style={styles.ContainerTextInput}>
 
-                    <TextInput
-                        style={styles.loginInputBox}
-                        onChangeText={setPassword_2}
-                        value={password_2}
-                        placeholder="Valider votre Mot de passe"
-                        secureTextEntry
-                    />
+                        <Image source={require('../../../assets/Profile.png')}/>
+                        <Image style={styles.line_V} source={require('../../../assets/Line_deco_v.png')}/>
+                        <TextInput
+                            onChangeText={setEmail}
+                            style={styles.loginInputBox}
+                            value={email}
+                            placeholder="Nom"
+                        />
+                    </View>
+                    <Image style={styles.line} source={require('../../../assets/Line_deco.png')}/>
+
+                    <View style={styles.ContainerTextInput}>
+
+                        <Image source={require('../../../assets/Profile.png')}/>
+                        <Image style={styles.line_V} source={require('../../../assets/Line_deco_v.png')}/>
+                        <TextInput
+                            onChangeText={setEmail}
+                            style={styles.loginInputBox}
+                            value={email}
+                            placeholder="Choisir un pseudo"
+                        />
+                    </View>
+                    <Image style={styles.line} source={require('../../../assets/Line_deco.png')}/>
+
+                    <View style={styles.ContainerTextInput}>
+
+                        <Image source={require('../../../assets/tel.png')}/>
+                        <Image style={styles.line_V} source={require('../../../assets/Line_deco_v.png')}/>
+                        <TextInput
+                            onChangeText={setEmail}
+                            style={styles.loginInputBox}
+                            value={email}
+                            placeholder="Numéro de téléphone"
+                        />
+                    </View>
+                    <Image style={styles.line} source={require('../../../assets/Line_deco.png')}/>
+
+                    <View style={styles.ContainerTextInput}>
+
+                        <Image source={require('../../../assets/Message.png')}/>
+                        <Image style={styles.line_V} source={require('../../../assets/Line_deco_v.png')}/>
+                        <TextInput
+                            onChangeText={setEmail}
+                            style={styles.loginInputBox}
+                            value={email}
+                            placeholder="Adresse e-mail"
+                        />
+                    </View>
+                    <Image style={styles.line} source={require('../../../assets/Line_deco.png')}/>
+
+                    <View style={styles.ContainerTextInput}>
+
+                        <Image source={require('../../../assets/mdp.png')}/>
+                        <Image style={styles.line_V} source={require('../../../assets/Line_deco_v.png')}/>
+
+                        <TextInput
+                            style={styles.loginInputBox}
+                            onChangeText={setPassword}
+                            value={password}
+                            placeholder="Mot de passe"
+                            secureTextEntry
+                        />
+
+                        <Image source={require('../../../assets/oeil.png')}/>
+                    </View>
+
+                    <Image style={styles.line} source={require('../../../assets/Line_deco.png')}/>
+
+                    <View style={styles.ContainerTextInput}>
+
+                        <Image source={require('../../../assets/mdp.png')}/>
+                        <Image style={styles.line_V} source={require('../../../assets/Line_deco_v.png')}/>
+
+                        <TextInput
+                            style={styles.loginInputBox}
+                            onChangeText={setPassword}
+                            value={password}
+                            placeholder="Répéter votre mot de passe"
+                            secureTextEntry
+                        />
+
+                        <Image source={require('../../../assets/oeil.png')}/>
+                    </View>
+
+                    <Image style={styles.line} source={require('../../../assets/Line_deco.png')}/>
                 </View>
+                <Text style={styles.textStyle}
+                >
+                    J'ai bien pris connaissance des CGU de l'application
+                </Text>
             </View>
 
             <View style={styles.container}>
                 <View style={styles.buttonContainer}>
-                    {buttonFactory.createSubmitButton(
-                        "Creer un compte",
-                        () => {
-                            navigation.navigate(routes.information);
-                        }
-                    )}
+
+                    <Pressable  style={styles.stylePressable} onPress={() => navigation.navigate(routes.information)}>
+
+                        <Image source={require('../../../assets/Arrow-Right.png')}/>
+
+                    </Pressable>
                 </View>
             </View>
+
+            <View style={styles.ContainerTextInput2}>
+                <Text style={{
+                    ...styles.colorizedText2,
+                    ...styles.textStyle
+                }}>
+                   Already a member?
+                </Text>
+                <Text style={{
+                    ...styles.colorizedText,
+                }}
+                      onPress={() => navigation.navigate(routes.login)}
+                > Sign in</Text>
+            </View>
+
         </View>
     )
 }
--- src/pages/register/register.style.js
+++ src/pages/register/register.style.js
@@ -14,20 +14,25 @@ export const styles = StyleSheet.create({
     },
     loginInputContainer: {
         width: "100%",
-        marginTop: 30,
+        marginTop: 20,
+        justifyContent: "space-evenly"
     },
     loginInputLabel: {
         fontSize: 16,
         marginBottom: 4,
     },
     loginInputBox: {
-        width: '100%',
+        width: '85%',
         padding: '10%',
         fontSize: 16,
 
     },
     colorizedText: {
+        fontWeight: "bold",
+        paddingTop: 10,
+        fontSize: 16,
         color: "#181D2D",
+
     },
     colorizedText2: {
         paddingTop: 10,
@@ -45,8 +50,51 @@ export const styles = StyleSheet.create({
     },
     buttonContainer: {
         width: "40%",
-        marginLeft: 10,
+        marginRight: 10,
         flex: 1,
-        justifyContent: 'flex-end'
-    }
+        justifyContent: 'flex-end',
+
+    },
+    line: {
+        alignItems: 'center',
+        width: "100%",
+        marginTop: -35,
+    },
+    ContainerTextInput: {
+        paddingBottom: 10,
+        flexDirection: 'row',
+        alignItems: 'center',
+    },
+
+    ContainerTextInput2: {
+
+        paddingBottom: 40,
+        flexDirection: 'row',
+    },
+    line_V: {
+        marginLeft: 10,
+    },
+    MDP: {
+        marginTop: 10,
+        textDecorationLine: 'underline',
+    },
+    pro:{
+        width: 1,
+        height: 1,
+    },
+
+    textStyle: {
+        paddingTop: 10,
+    },
+    stylePressable: {
+        height: 50,
+        width: 50,
+        backgroundColor: "#61E5F8",
+        borderRadius: 30,
+        right: 10,
+        top: 60,
+        justifyContent: 'center',
+        alignItems: 'center',
+        position : 'absolute',
+    },
 });
